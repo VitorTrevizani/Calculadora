f6a210cb4ff8cc9403288f6456dc8010daf2916d
VitorTrevizani <vitoralextrevizani6@gmail.com>
2025-10-02 21:15:20 -0300

    primeiros cálculos
    Já é possível efetuar cálculos simples, com o resultado sendo mostrado na tela da calculadora. Adicionei mais uma session e uma estrutura condicional com jinja2 no html para que fosse possível a exibição do resultado.

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ from flask import Flask, render_template, request, url_for, redirect, session
 app = Flask(__name__)
 app.secret_key = 'siiiiiuuu'
 
+
 @app.route('/')
 def index():
-   return render_template('interface.html')
+   if "expressao" not in session:
+      session["expressao"] = ""
+   if "resultado" not in session:
+      session["resultado"] = ""
+
+   
+   return render_template('interface.html', exp = session["expressao"], res = session["resultado"] )
 
 
 @app.route('/form', methods=['POST'])
 def form():
    botao = request.form['botao']
    if botao == "=":
-       resul = eval(session["expressao"])
-       print(resul)
+       resul = eval(session["expressao"])  #calcula o resultado
+       session["resultado"] = resul        #joga na sessão pra renderizar na pagina
+       session["expressao"] = ''  
+       botao = ''         #limpa a expressão atual
+   else:
+      session["resultado"] = "none"  
        
-   if "expressao" not in session:
-        session["expressao"] = ""
 
    session["expressao"] += botao
    print(session["expressao"])
